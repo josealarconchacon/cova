2d9bf080a7d13fb3cb611508b148cb8901e0213f
josealarconchacon <jose.alarcon.chacon@gmail.com>
2026-02-22 21:18:40 -0500

    route into and out of the auth

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,16 +1,6 @@
-import { useEffect } from "react";
 import { Stack } from "expo-router";
-import { router } from "expo-router";
-import { supabase } from "../../lib/supabase";
 
 export default function AuthLayout() {
-  useEffect(() => {
-    // If user is already logged in, skip auth screens
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      if (session) router.replace("/(tabs)/");
-    });
-  }, []);
-
   return (
     <Stack
       screenOptions={{
