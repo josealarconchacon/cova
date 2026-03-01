0c5e653c3d1143324f20c814cfc69a1b47005d34
josealarconchacon <jose.alarcon.chacon@gmail.com>
2026-02-28 23:04:48 -0500

    Update index/entry

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,6 @@
 import { View, ActivityIndicator } from "react-native";
 import { Colors } from "../constants/theme";
 
-// This is the app's true entry point. Expo-router renders this first
-// while _layout.tsx resolves fonts + auth state, then navigates away.
 export default function IndexGate() {
   return (
     <View
